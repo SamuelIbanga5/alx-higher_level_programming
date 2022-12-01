7889c16e007fa91cca031b8ff23315e01fd2d94e
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-01 08:39:53 +0100

    Created python script for infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+argv_length = len(argv)
+sum_of_arguments = 0
+for i in range(1, argv_length):
+    if not argv[i].isalpha():
+        num = int(argv[i])
+        sum_of_arguments += num
+print("{}".format(sum_of_arguments))
