877c6395bd0c3ddead476b1b5f28edc756b715fc
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-01 16:23:38 +0100

    Updated all .py files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-argv_length = len(argv)
-argument = ""
-if argv_length == 1:
-    print("{} arguments.".format(0))
-elif argv_length > 2:
-    print("{} arguments:".format(argv_length - 1))
-    for i in range(1, argv_length):
-        print("{}: {}".format(i, argv[i]))
-elif argv_length == 2:
-    print("{} argument:".format(argv_length - 1))
-    for i in range(1, argv_length):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    argv_length = len(argv)
+    argument = ""
+    if argv_length == 1:
+        print("{} arguments.".format(0))
+    elif argv_length > 2:
+        print("{} arguments:".format(argv_length - 1))
+        for i in range(1, argv_length):
+            print("{}: {}".format(i, argv[i]))
+    elif argv_length == 2:
+        print("{} argument:".format(argv_length - 1))
+        for i in range(1, argv_length):
+            print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-argv_length = len(argv)
-sum_of_arguments = 0
-for i in range(1, argv_length):
-    if not argv[i].isalpha():
-        num = int(argv[i])
-        sum_of_arguments += num
-print("{}".format(sum_of_arguments))
+if __name__ == "__main__":
+    from sys import argv
+    argv_length = len(argv)
+    sum_of_arguments = 0
+    for i in range(1, argv_length):
+        if not argv[i].isalpha():
+            num = int(argv[i])
+            sum_of_arguments += num
+    print("{}".format(sum_of_arguments))
