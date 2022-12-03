bfa5a726bd3b0d7ae2851b7508d2aa189490195d
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-03 23:41:16 +0100

    Updated matrix file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
-            if j != 2:
+            if j is not i[len(i) - 1]:
                 print("{}".format(i[j]), end=" ")
             else:
                 print("{}".format(i[j]), end="")
