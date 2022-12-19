76e809e8b1591604a4dc3d50838474fa10a93314
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-19 22:39:43 +0100

    Created python script file for safe printing list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,15 @@
-def _len(iterable):
-    if hasattr(iterable, "__iter__"):
-        count = 0
-        for i in iterable:
-            count += 1
-        return count
-    else:
-        return
+#!/usr/bin/python3
+
 
 def safe_print_list(my_list=[], x=0):
-    for index in range(x):
+    count = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[index]), end="")
+            print('{}'.format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
         except IndexError:
             break
-    print("")
-    if x <= _len(my_list):
-        return x
-    else:
-        return _len(my_list)
+    print()
+    return count
