a023753f4c991761df73fbe8e356f5b8228e0e85
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-20 20:05:47 +0100

    Updated script file for execution

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """
-        An empty Square class.
-    """
+    """An empty Square class."""
     pass
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
