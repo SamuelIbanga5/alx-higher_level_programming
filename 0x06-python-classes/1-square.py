7cce663aca0f846e3efbf0cbb2ea0113ae20db58
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-20 21:27:21 +0100

    Created python script for square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""My second python class (Square)"""
+class Square:
+    """Square class for creating squares with different sizes"""
+    def __init__(self, size):
+        """__init__ method is called automatically to instantiate
+        a class to a object.
+
+        Args:
+            size (int): Specifies the size of square.
+        """
+        self.size = size
+
+
+if __name__ == '__main__':
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
