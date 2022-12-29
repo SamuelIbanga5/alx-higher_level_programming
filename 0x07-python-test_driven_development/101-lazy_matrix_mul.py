b8f09b5c080e2744af3c3a012799c306831fff76
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-30 00:04:35 +0100

    Created python scrpt file for lazy matrix multiplicaiton

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 101-lazy_matrix_mul
+Contains method to multiply matrix using numpy module (pip3 install numpy)
+https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.matmul.html
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Returns matrix multiplied"""
+    return numpy.matmul(m_a, m_b)
+
+if __name__ == "__main__":
+    print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+    print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
