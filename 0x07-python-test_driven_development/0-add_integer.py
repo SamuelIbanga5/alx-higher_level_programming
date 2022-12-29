809ce6c1cfb1271b682d7f9b05e371ddbc2b54ef
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-29 23:33:52 +0100

    Created python script file for dividing matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Module 0-add_integer
 Contains one method that returns an int sum
@@ -17,3 +18,18 @@ def add_integer(a, b=98):
         sum = int(a) + int(b)
     return sum
 
+if __name__ == "__main__":
+    add_integer = __import__('0-add_integer').add_integer
+
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
