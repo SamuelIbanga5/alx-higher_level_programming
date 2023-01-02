b334aefb8316868f701e7d60d06178c986a4647d
Samuel Ibanga <ibangajnr27@gmail.com>
2023-01-02 15:24:08 +0100

    Created directory for more python class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""Python Rectangle class"""
+class Rectangle:
+    """Rectangle class that creates rectangles of different sizes."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+# More Python Object and classes
