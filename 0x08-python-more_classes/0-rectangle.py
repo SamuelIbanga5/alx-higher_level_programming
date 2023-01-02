7d375795e6d524aee32ff800f3d2ed76486c48ab
Samuel Ibanga <ibangajnr27@gmail.com>
2023-01-02 15:27:53 +0100

    Changed python script file to executable

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,3 +4,8 @@
 class Rectangle:
     """Rectangle class that creates rectangles of different sizes."""
     pass
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
