69d5b9972b3c5131e88efd7f02c684ad2bbe82f3
Samuel Ibanga <ibangajnr27@gmail.com>
2023-01-16 11:48:07 +0100

    serialize base instance'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,8 @@ Returns list of instances
 Saves to CSV and loads from CSV file
 """
 
+
+import json
 class Base:
     """
     defines class Base
@@ -34,4 +36,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to_json_string static method that returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
