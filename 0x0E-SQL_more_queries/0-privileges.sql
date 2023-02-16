a3738b735276501a37115c9ccdf84946eb0e6add
Samuel Ibanga <ibangajnr27@gmail.com>
2023-02-16 02:10:20 +0100

    privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- SQL script that lists all privileges of the MYSQL users user_0d_1 and user_0d_2 on server in localhost.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
