a2914ddb5b82e7f11cb6b5fb7f95a2284a13d89e
Samuel Ibanga <ibangajnr27@gmail.com>
2023-02-16 03:40:44 +0100

    updated unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 --SQL Script that creates the table unique_id on MYSQL server.
 CREATE TABLE IF NOT EXISTS `unique_id` (
-	`id` INT	DEFAULT 1 UNIQUE,
-       	`name` VARCHAR(256)
+	`id` 	INT		DEFAULT 1 UNIQUE,
+       	`name` 	VARCHAR(256)
 );
