bd0b6b5b2da2923dcddf9e887fee7fb557b22a47
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-14 21:40:01 +0100

    square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,19 @@
 #!/usr/bin/node
 const OldSquare = require('./5-square');
 class Square extends OldSquare {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     if (c === undefined) {
-      for (let height = 0; height < this.size; height++) {
+      for (let height = 0; height < this.height; height++) {
         let x = 'X';
-        for (let width = 0; width < this.size - 1; width++) {
+        for (let width = 0; width < this.width - 1; width++) {
           x += 'X';
         }
         console.log(x);
       }
     } else {
-      for (let height = 0; height < this.size; height++) {
+      for (let height = 0; height < this.height; height++) {
         let x = 'c';
-        for (let width = 0; width < this.size - 1; width++) {
+        for (let width = 0; width < this.width - 1; width++) {
           x += 'c';
         }
         console.log(x);
