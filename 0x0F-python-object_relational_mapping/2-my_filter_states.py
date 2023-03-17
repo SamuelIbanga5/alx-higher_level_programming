fb1622632b899005e74dfbc426dcf59c11e7444f
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-17 21:28:19 +0100

    updated code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Script that takes in an argument and displays all values in the states table
-# of hbtn_0e_0_usa where name matches the argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                               <mysql password> \
+                               <database name> \
+                               <state name searched>
+"""
 import sys
 import MySQLdb
 
