f668bdce402fc9ed443ddd3acf1fae83b8131b40
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-17 21:07:14 +0100

    updated to pycodestyle

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But tis time a script that is safe from MySQL injections.
+# Script that takes in arguments and displays all values\
+# in the states table of hbtn_0e_0_usa where name matches\
+# the argument. But tis time a script that is safe from MySQL injections.
 # Usage: ./3-my_safe_filter_states.py <mysql username> \
 #                                     <mysql password> \
 #                                     <database_name> \
@@ -15,7 +17,8 @@ if __name__ == "__main__":
     state_name = args[4]
     db = MySQLdb.connect(user=user, passwd=passwd, db=dbname)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM {dbname}.states WHERE (name = '@{state_name}')")
+    cur.execute(f"SELECT * FROM {dbname}.states WHERE\
+            (name = '@{state_name}')")
     rows = cur.fetchall()
     for row in rows:
         print(row)
