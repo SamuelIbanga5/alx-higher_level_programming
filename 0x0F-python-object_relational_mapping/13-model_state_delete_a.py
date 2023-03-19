e9c72901e99ced6b06d2b32eb5254d9fcc35b38c
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-19 01:59:48 +0100

    updated code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
     session = Session()
-    states = session.query(State).filter(State.name.contains('a')).all()
+    states = session.query(State).filter(State.name.contains('a'))
     for state in states:
         session.delete(state)
         session.commit()
