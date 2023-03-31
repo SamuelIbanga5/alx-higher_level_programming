4985b1499d8a43a22294d1592630e13265930e8a
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-31 09:31:22 -0700

    updated code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,9 +8,17 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     if len(args) == 1:
-        q = ""
+        data = {"q": ""}
+        print("No result")
     else:
         q = args[1]
         data = {"q": q}
         r = requests.post("http://0.0.0.0:5000/search_user", data)
-        print(r.text)
+        try:
+            json_response = r.json()
+            if json_response == {}:
+                print("No result")
+            else:
+                print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        except ValueError:
+            print("Not a valid JSON")
